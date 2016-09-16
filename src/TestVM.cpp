8084a0a948ff800073f8ff8d4b45bf77852d8c2d
Brian Schnepp <bschneppdev@gmail.com>
2016-09-15 20:12:57 -0700

    Fixed file structure, I think. I do apologize, this is my first time using Git from command line, not with some tool.

---
+++ src/TestVM.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TestVM.cpp
+// Author      : Brian Schnepp
+// Version     :
+// Copyright   : See 'LICENSE' in root directory of this project.
+// Description : A simple virtual machine for testing things out.
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string readLine = std::to_string(0xA3);
+	std::cout << readLine << std::endl;
+	return 0;
+}
