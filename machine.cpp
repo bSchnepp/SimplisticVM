07cd3adb549438db63b5687c96e270beab3685a2
Brian Schnepp <bSchnepp@users.noreply.github.com>
2016-09-15 19:53:46 -0700

    Add files via upload

---
+++ machine.cpp
@@ -0,0 +1,20 @@
+/*
+ * machine.cpp
+ *
+ *  Created on: Sep 15, 2016
+ *      Author: brian-dev
+ */
+
+#include "machine.h"
+
+Machine::Machine(int64_t corecount)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Machine::~Machine()
+{
+	// TODO Auto-generated destructor stub
+}
+
