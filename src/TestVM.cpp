b0b43dd2bc42d0789aea73263018b83e76789371
Brian Schnepp <bSchnepp@users.noreply.github.com>
2016-12-09 21:57:02 -0800

    Update TestVM.cpp

--- src/TestVM.cpp
+++ src/TestVM.cpp
@@ -3,7 +3,7 @@
 // Author      : Brian Schnepp
 // Version     :
 // Copyright   : See 'LICENSE' in root directory of this project.
-// Description : A simple virtual machine for testing things out. Goal is a *very* simplified x86-like instruction set, which makes it easy to rewrite assembly for real processors.
+// Description : A simple virtual machine for testing things out. Goal is a *very* simplified x86-like assembly language, which makes it easy to rewrite assembly for real processors.
 // (Assembler should be more or less close to NASM Intel for x86, with our registers and stuff.)
 // Should still be CISC, though.
 //============================================================================
